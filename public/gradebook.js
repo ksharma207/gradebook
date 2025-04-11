4e23c92b4bd73dfe5bafea2dd4d381cbbbfa4b3e
ksharma207 <khushalsharm07@gmail.com>
2025-04-11 17:02:25 -0400

    Create gradebook.js

---
+++ public/gradebook.js
@@ -0,0 +1,13 @@
+function fetchGradeData() {
+  console.log("fetchGradeData called");
+  // This function will fetch grade data in the future
+}
+
+function populateGradebook(data) {
+  console.log("populateGradebook called with data:", data);
+  // This function will populate the gradebook in the future
+}
+
+// Call the stub functions for now
+fetchGradeData();
+populateGradebook([]);
